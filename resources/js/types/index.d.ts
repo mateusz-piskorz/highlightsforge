d5fe5f3d3a742813da747c57a0f0d96e0aba7850
epickaowca <epickaowca@Epickas-Laptop.local>
2025-12-12 12:36:43 +0100

    refactor-12-12-2025

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,20 +1,5 @@
-import { InertiaLinkProps } from '@inertiajs/vue3';
-import type { LucideIcon } from 'lucide-vue-next';
-
 export interface Auth {
     user: User;
-}
-
-export interface BreadcrumbItem {
-    title: string;
-    href: string;
-}
-
-export interface NavItem {
-    title: string;
-    href: NonNullable<InertiaLinkProps['href']>;
-    icon?: LucideIcon;
-    isActive?: boolean;
 }
 
 export type AppPageProps<
@@ -34,5 +19,3 @@ export interface User {
     updated_at: string;
     two_factor_confirmed_at: string | null;
 }
-
-export type BreadcrumbItemType = BreadcrumbItem;
