46e66b9a79cfb1ca0282322404f56099a9f6cd9d
epickaowca <epickaowca@Epickas-Laptop.local>
2025-12-12 12:36:43 +0100

    refactor-12-12-2025

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
-Route::get('/settings', fn () => Inertia::render('settings/page'));
-
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->middleware(['api'])->name('home');
-
-Route::get('/upload', function () {
-    return Inertia::render('upload-page');
-})->name('upload');
-
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
-// ->middleware([])
-// auth', 'verified
-require __DIR__.'/settings.php';
+Route::get('/', fn() => Inertia::render('page'));
+Route::get('/settings', fn() => Inertia::render('settings/page'));
+Route::get('/upload', fn() => Inertia::render('upload/page'));
