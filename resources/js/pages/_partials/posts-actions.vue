1ca383ad7f0859f66bd4bf6d66a5607466400c1d
epickaowca <epickaowca@Epickas-Laptop.local>
2025-12-31 14:24:39 +0100

    fix-linting

--- resources/js/pages/_partials/posts-actions.vue
+++ resources/js/pages/_partials/posts-actions.vue
@@ -1,12 +1,6 @@
 <script setup lang="ts">
 import NewPostBtn from '@/components/new-post-btn.vue';
 import PostSortInput from '@/components/post-sort-input.vue';
-import { useCommonDialogs } from '@/lib/composables/useCommonDialogs';
-import { usePage } from '@inertiajs/vue3';
-
-const { user } = usePage().props.auth;
-
-const { setOpen } = useCommonDialogs();
 </script>
 
 <template>
